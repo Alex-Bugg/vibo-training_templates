a6cf6d270010bf4f40183851ac5e82b324e427a8
AlexBugg <alexand.from.ua@gmail.com>
2020-05-30 15:33:25 +0300

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,16 @@ toggleBurger.onclick = function () {
   toggleBurger.classList.toggle("toggle_active");
   navHeader.classList.toggle("toggle_active");
 };
+
+let accBlock = document.querySelectorAll('.accordion_item');
+for (var i = 0; i < accBlock.length; i++) {
+  accBlock[i].onclick = function () {
+    this.classList.toggle('accordion_active');
+    var content = this.querySelector('.accordion__content');
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px'
+    }
+  }
+};
